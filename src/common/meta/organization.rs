0ea6601005beda643a61d3dbf77bc3a84069d765
Harsh Mahajan <007harshmahajan@gmail.com>
2026-01-21 17:53:13 +0530

    fix

--- src/common/meta/organization.rs
+++ src/common/meta/organization.rs
@@ -301,6 +301,10 @@ fn default_enable_streaming_search() -> bool {
     false
 }
 
+fn default_usage_stream_enabled() -> bool {
+    false
+}
+
 #[cfg(feature = "enterprise")]
 fn default_claim_parser_function() -> String {
     "".to_string()
@@ -330,6 +334,8 @@ pub struct OrganizationSettingPayload {
     pub dark_mode_theme_color: Option<String>,
     #[serde(skip_serializing_if = "Option::is_none")]
     pub max_series_per_query: Option<usize>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub usage_stream_enabled: Option<bool>,
     #[cfg(feature = "enterprise")]
     #[serde(skip_serializing_if = "Option::is_none")]
     pub claim_parser_function: Option<String>,
@@ -363,6 +369,8 @@ pub struct OrganizationSetting {
     pub dark_mode_theme_color: Option<String>,
     #[serde(skip_serializing_if = "Option::is_none")]
     pub max_series_per_query: Option<usize>,
+    #[serde(default = "default_usage_stream_enabled")]
+    pub usage_stream_enabled: bool,
     #[cfg(feature = "enterprise")]
     #[serde(default = "default_claim_parser_function")]
     pub claim_parser_function: String,
@@ -394,6 +402,7 @@ impl Default for OrganizationSetting {
             light_mode_theme_color,
             dark_mode_theme_color,
             max_series_per_query: None,
+            usage_stream_enabled: default_usage_stream_enabled(),
             #[cfg(feature = "enterprise")]
             claim_parser_function: default_claim_parser_function(),
         }
